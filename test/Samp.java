a4af9be2d45bb0c1725f23bf8d63a6f53fe7363b
itzmejayaraj <jayarajpec@gmail.com>
2022-01-20 11:36:02 +0530

    new folder

---
+++ test/Samp.java
@@ -0,0 +1,38 @@
+package org.test;
+
+import java.security.KeyStore.Entry;
+import java.util.*;
+
+public class Samp {
+	
+	public static void main(String[] args) {
+		int c=0;
+		Scanner s =new Scanner(System.in);
+		System.out.print("A=");
+		int a = s.nextInt();
+		System.out.print("B=");
+		int b = s.nextInt();
+		
+			
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+	}
+	}
+
+			
+	
+			
+			
+			
+		
+				
+				
+			
+		
+		
+		
+	
+
